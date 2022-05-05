ad30576732f2dd98b4e1e6d6c849cb71977730da
ahyeon0508 <ahyeon_0508@naver.com>
2022-05-05 21:45:11 +0900

    [백준] k번째 수
    이분탐색

---
+++ 백준/이분 탐색/code/K번째 수 - 2.py
@@ -0,0 +1,20 @@
+N = int(input())
+k = int(input())
+
+start = 1
+end = k
+
+while start <= end:
+    mid = (start + end) // 2
+
+    cnt = 0
+    for i in range(1, N+1):
+        cnt += min(mid // i, N) # mid 이하의 i의 배수 or 최대 N
+
+    if cnt >= k:
+        answer = mid
+        end = mid - 1
+    else:
+        start = mid + 1
+
+print(answer)
